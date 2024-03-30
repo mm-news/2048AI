9d3735f350dc812db8e2a3df318c71001bad2b7f
mmtv <mmtv.news.tw@gmail.com>
2024-03-30 19:19:33 +0800

    Finished the game

--- game/game.py
+++ game/game.py
@@ -1,2 +1,21 @@
-def start():
-    pass
+from game.classes import Field
+
+field = Field(4)
+
+while True:
+    field.add_new()
+    for i in field.grld:
+        print(i)
+    reply = input("Enter a direction: ")
+    match reply:
+        case "0":
+            field.move(0)
+        case "1":
+            field.move(1)
+        case "2":
+            field.move(2)
+        case "3":
+            field.move(3)
+        case _:
+            break
+    print()
